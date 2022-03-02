3947884490603bfe4f7d6c9caf0c3d07616c411c
Md. Shofiul Islam <ytshofiul@gmail.com>
2022-03-02 22:01:29 +0600

    MealSummary added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Fragment } from "react";
 
 import Header from './components/Header/Header';
+import MealSummary from "./components/Meal/MealSummary";
 
 const App = props =>{
   return( 
   <Fragment>
     <Header/>
+    <MealSummary/>
   </Fragment>);
 }
 export default App;
