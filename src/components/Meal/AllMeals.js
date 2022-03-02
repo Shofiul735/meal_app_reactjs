e1d896bc6a88f38337b95766f0b52bd8a05f8ae6
Md. Shofiul Islam <ytshofiul@gmail.com>
2022-03-02 23:49:33 +0600

    MealDetails component added in the AllMeals

--- src/components/Meal/AllMeals.js
+++ src/components/Meal/AllMeals.js
@@ -1,42 +1,53 @@
-import classes from './AllMeals.module.css';
+import Card from "../UI/Card/Card";
+import MealDetails from "./MealDetails";
+import classes from "./AllMeals.module.css";
 
 const DUMMY_MEALS = [
-    {
-      id: "m1",
-      name: "Sushi",
-      description: "Finest fish and veggies",
-      price: 22.99,
-    },
-    {
-      id: "m2",
-      name: "Schnitzel",
-      description: "A german specialty!",
-      price: 16.5,
-    },
-    {
-      id: "m3",
-      name: "Barbecue Burger",
-      description: "American, raw, meaty",
-      price: 12.99,
-    },
-    {
-      id: "m4",
-      name: "Green Bowl",
-      description: "Healthy...and green...",
-      price: 18.99,
-    },
-  ];
-
+  {
+    id: "m1",
+    name: "Sushi",
+    description: "Finest fish and veggies",
+    price: 22.99,
+  },
+  {
+    id: "m2",
+    name: "Schnitzel",
+    description: "A german specialty!",
+    price: 16.5,
+  },
+  {
+    id: "m3",
+    name: "Barbecue Burger",
+    description: "American, raw, meaty",
+    price: 12.99,
+  },
+  {
+    id: "m4",
+    name: "Green Bowl",
+    description: "Healthy...and green...",
+    price: 18.99,
+  },
+];
 
 const AllMeals = (props) => {
-    return (
-      <div className={classes['all-meals']}>
-        <ul className={classes['unorder-list']}>
-            {DUMMY_MEALS.map((item)=>{
-                return <li key={item.id}>{item.name}</li>
-            })}
-        </ul>
-      </div>
-    );
-}
+  return (
+    <div className={classes["all-meals"]}>
+      <ul className={classes["unorder-list"]}>
+        {DUMMY_MEALS.map((item) => {
+          return (
+            <li key={item.id}>
+              <Card>
+                <MealDetails
+                  name={item.name}
+                  description={item.description}
+                  price={item.price}
+                />
+              </Card>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 export default AllMeals;
