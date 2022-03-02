7228dca8caae0c4190a986eed7562522f99df81b
Md. Shofiul Islam <ytshofiul@gmail.com>
2022-03-02 22:10:04 +0600

    AllMeals component added
    This component will show all the available items

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Fragment } from "react";
 
 import Header from './components/Header/Header';
 import MealSummary from "./components/Meal/MealSummary";
+import AllMeals from "./components/Meal/AllMeals";
 
 const App = props =>{
   return( 
   <Fragment>
     <Header/>
     <MealSummary/>
+    <AllMeals/>
   </Fragment>);
 }
 export default App;
---
+++ src/components/Meal/AllMeals.js
@@ -0,0 +1,40 @@
+import classes from './AllMeals.module.css';
+
+const DUMMY_MEALS = [
+    {
+      id: "m1",
+      name: "Sushi",
+      description: "Finest fish and veggies",
+      price: 22.99,
+    },
+    {
+      id: "m2",
+      name: "Schnitzel",
+      description: "A german specialty!",
+      price: 16.5,
+    },
+    {
+      id: "m3",
+      name: "Barbecue Burger",
+      description: "American, raw, meaty",
+      price: 12.99,
+    },
+    {
+      id: "m4",
+      name: "Green Bowl",
+      description: "Healthy...and green...",
+      price: 18.99,
+    },
+  ];
+
+
+const AllMeals = (props) => {
+    return (
+        <ul>
+            {DUMMY_MEALS.map((item)=>{
+                return <li key={item.id}>{item.name}</li>
+            })}
+        </ul>
+    );
+}
+export default AllMeals;
