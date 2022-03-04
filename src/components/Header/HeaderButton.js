b4974c2afd0a212bd171fcd08779c6b9ff9a0ccf
Md. Shofiul Islam <ytshofiul@gmail.com>
2022-03-04 12:17:08 +0600

    header amination and totalAmount added

--- src/components/Header/HeaderButton.js
+++ src/components/Header/HeaderButton.js
@@ -1,11 +1,27 @@
+import { useState,useEffect,useContext } from 'react';
 
+import CartContext from '../../context/cart-context';
 import classes from './HeaderButton.module.css';
 
 const HeaderButton = (props) => {
+    const context = useContext(CartContext);
+    const [amination,setAnimation] = useState(false);
+    const items = context.items;
+    const buttonClass = `${classes['badge']} ${ amination ? classes['bump']:''}`;
+    useEffect(()=>{
+        setAnimation(true);
+        const timer = setTimeout(()=>{
+            setAnimation(false);
+        },300);
+        return ()=>{
+            clearTimeout(timer);
+        }
+    },[items]);
+
     return (
         <button className={classes['button']}>
             <span className={classes['cart']}>Your Cart</span>
-            <span className={classes['badge']}>3</span>
+            <span className={buttonClass}>{context['totalAmount']}</span>
         </button>
     );
 }
