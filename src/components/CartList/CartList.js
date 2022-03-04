9c012e11d29e8ffbc99e9583fbdcb9e81d0b5524
Md. Shofiul Islam <ytshofiul@gmail.com>
2022-03-04 21:14:48 +0600

    Scrollable list implemented

--- src/components/CartList/CartList.js
+++ src/components/CartList/CartList.js
@@ -1,29 +1,42 @@
 import { useContext } from "react";
 
-import Modal from '../UI/Modal/Modal';
-import Cart from './Cart';
-import CartContext from '../../context/cart-context';
+import Modal from "../UI/Modal/Modal";
+import Cart from "./Cart";
+import CartContext from "../../context/cart-context";
 
-import classes from './CartList.module.css';
+import classes from "./CartList.module.css";
 
 const CartList = (props) => {
-    const context = useContext(CartContext);
-    const items = [...context.items];
-    const list = items.map((item)=><Cart
-        key={item.id}
-        name={item.name}
-        price={item.price}
-        count= {item.count}
-        onAdd={context.addItem.bind(null,item)}
-        onRemove = {context.removeItem.bind(null,item.id)}
-    />);
-    return <Modal onRemoveCart={props.onRemoveCart}>
-        {list}
-        <div className={classes['button']}>
-            <button onClick={props.onRemoveCart} className={`${classes['btn']} ${classes['order']}`}>Order</button>
-            <button onClick={props.onRemoveCart} className={`${classes['btn']} ${classes['cancel']}`}>Cancel</button>
-        </div>
+  const context = useContext(CartContext);
+  const items = [...context.items];
+  const list = items.map((item) => (
+    <Cart
+      key={item.id}
+      name={item.name}
+      price={item.price}
+      count={item.count}
+      onAdd={context.addItem.bind(null, item)}
+      onRemove={context.removeItem.bind(null, item.id)}
+    />
+  ));
+  return (
+    <Modal onRemoveCart={props.onRemoveCart}>
+      <div className={classes['cart-list']}>{list}</div>
+      <div className={classes["button"]}>
+        <button
+          onClick={props.onRemoveCart}
+          className={`${classes["btn"]} ${classes["order"]}`}
+        >
+          Order
+        </button>
+        <button
+          onClick={props.onRemoveCart}
+          className={`${classes["btn"]} ${classes["cancel"]}`}
+        >
+          Cancel
+        </button>
+      </div>
     </Modal>
-
-}
+  );
+};
 export default CartList;
