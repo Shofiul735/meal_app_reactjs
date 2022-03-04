8ba2aa9e7fb06c2593cff7b40e60d5e0813a60bf
Md. Shofiul Islam <ytshofiul@gmail.com>
2022-03-04 18:50:05 +0600

    Modal created for the project

---
+++ src/components/UI/Modal/Model.js
@@ -0,0 +1,23 @@
+import { Fragment } from "react";
+import { createPortal } from "react-dom";
+
+import classes from "./Modal.module.css";
+
+const Backdrop = (props) => {
+  return <div className={classes["backdrop"]}></div>;
+};
+
+const ModalOverlay = (props) => {
+    return <div className={classes['modalOverlay']}>
+        {props.children}
+    </div>
+}
+
+const portalElement = document.getElementById("modal");
+
+const Modal = (props) => {
+    return <Fragment>
+        {createPortal(<Backdrop/>,portalElement)}
+        {createPortal(<ModalOverlay>{props.children}</ModalOverlay>,portalElement)}
+    </Fragment>
+}
