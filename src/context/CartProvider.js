770edc935e79fdcf355b30cf1ef59e4f99376ed2
Md. Shofiul Islam <ytshofiul@gmail.com>
2022-03-04 11:54:49 +0600

    CartProvider created

---
+++ src/context/CartProvider.js
@@ -0,0 +1,15 @@
+import CartContext from "./cart-context";
+
+const CartProvider = (props) => {
+  const addItemHandler = (item) => {};
+  const removeItemHandler = (id) => {};
+
+  const value = {
+    items: [],
+    totalAmount: 0,
+    addItem: addItemHandler,
+    removeItem: removeItemHandler,
+  };
+  return <CartContext.Provider>{props.children}</CartContext.Provider>;
+};
+export default CartProvider;
